99fa098b907a6edbb55511c2d0543676fcee539a
Diloan Silva <diloan.silva@gmail.com>
2018-01-25 08:15:08 -0300

    Numero primo

---
+++ desafios-java/src/desafios/java/programmr03/Flow.java
@@ -0,0 +1,16 @@
+package desafios.java.programmr03;
+
+public class Flow {
+	// Verifica se o numero dado é primo ou não
+	  public static void main(String[] args) {
+
+		    int num = 11;
+		    String prime;
+		    
+		  if(num % 2 != 0 && num % 3 != 0){
+			  System.out.println("The number is prime.");
+		  }else{
+			  System.out.println("The number not is prime.");
+		  }
+}
+}
