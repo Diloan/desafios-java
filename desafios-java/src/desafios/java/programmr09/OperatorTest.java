a7122242939817d26bba767476a158b7f9eacebe
Diloan Silva <diloan.silva@gmail.com>
2018-01-25 09:08:51 -0300

    Multiplica dois números

---
+++ desafios-java/src/desafios/java/programmr09/OperatorTest.java
@@ -0,0 +1,22 @@
+package desafios.java.programmr09;
+
+public class OperatorTest {
+	
+	//Multiplica dois números
+	public static void main(String[] args) {
+		int number, number2;  
+
+		number = 15;
+		number2 = 9;
+
+		int calculatedResult = 0; //variable to hold answer
+		
+		calculatedResult = number * number2;
+		
+		System.out.println("Muliplication of the 2 numbers is:"); 
+
+		System.out.println(calculatedResult);
+
+	}
+
+}
